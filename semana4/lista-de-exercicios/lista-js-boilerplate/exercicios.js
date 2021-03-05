48bfc7bb9a2ae6bec5274989b896693c117e4f20
Danielle Costa <dascott.pucca@gmail.com>
2021-03-05 20:56:30 -0300

    Lsita de exercicios finalizados

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -85,43 +85,120 @@ function checaTriangulo(a, b, c) {
 // Exercício 9
 
 function comparaDoisNumeros(num1, num2) {
-   // implemente sua lógica aqui
+   let maiorNumero
+   let menorNumero
+   let maiorDivisivelporMenor
+
+   if(num1 > num2){
+      maiorNumero = num1
+      menorNumero = num2
+   } else  {
+      maiorNumero = num2
+      menorNumero = num1
+   }
+
+   maiorDivisivelporMenor = maiorNumero % menorNumero === 0;
+
+   let diferenca = maiorNumero - menorNumero
+
+   return {
+      maiorNumero: maiorNumero,
+      maiorDivisivelporMenor: maiorDivisivelporMenor,
+      diferenca: diferenca,
+   }
 }
 
 // Exercício 10
 
 function segundoMaiorEMenor(array) {
-   // implemente sua lógica aqui
+   let menor = Infinity
+   let maior = 0
+   let segundoMenor = Infinity
+   let segundoMaior = 0
+   let indiceMenor = 0
+   let indiceMaior = 0
+   let novoArray = []
+
+   for(let i of array){
+      if (i < menor){
+         menor=i
+         indiceMenor = array.indexOf(menor)
+      }
+      if (i > maior){
+         maior = i
+         indiceMaior = array.indexOf(maior)
+      }
+   }
+   array.splice(indiceMaior,1)
+   array.splice(indiceMenor, 1)
+
+   for (let i of array){
+      if (i < segundoMenor){
+         segundoMenor = i
+      } if (i > segundoMaior){
+         segundoMaior = i
+      } 
+   }
+   novoArray.push(segundoMaior)
+   novoArray.push(segundoMenor)
+return novoArray
 }
 
 //Exercício 11
 
 function ordenaArray(array) {
-   // implemente sua lógica aqui
+   let ordena =array.length
+   for (let i = 0; i < ordena; i++){
+      for (let j = 0; j < ordena; j++){
+         if(array[j] > array[j + 1]){
+            let abc = array[j]
+            array[j] = array[j + 1]
+            array[j + 1] = abc
+         }
+      }
+   }return array
 }
 
 // Exercício 12
 
 function filmeFavorito() {
-   // implemente sua lógica aqui
+   let filme ={
+      nome: 'O Diabo Veste Prada',
+      ano: 2006,
+      diretor: 'David Frankel',
+      atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+    
+ } 
+ return filme
 }
 
 // Exercício 13
 
 function imprimeChamada() {
-   // implemente sua lógica aqui
+   const output = 'Venha assistir ao filme O Diabo Veste Prada, de 2006, dirigido por David Frankel e estrelado por Meryl Streep, Anne Hathaway, Emily Blunt, Stanley Tucci.'
+   return output
 }
 
 // Exercício 14
 
 function criaRetangulo(lado1, lado2) {
-   // implemente sua lógica aqui
+   const retangulo ={
+      largura : lado1,
+      altura : lado2,
+      perimetro: 2 * (lado1 + lado2),
+      area: lado1 * lado2,
+   }
+   return retangulo
 }
 
 // Exercício 15
 
 function anonimizaPessoa(pessoa) {
-   // implemente sua lógica aqui
+   return {
+      ...pessoa,
+      nome:"ANÔNIMO"
+   }
+
 }
 
 // Exercício 16
@@ -136,32 +213,54 @@ const arrayDePessoas = [
 // Exercício 16, letra A
 
 function maioresDe18(arrayDePessoas) {
-   // implemente sua lógica aqui
+   const novo = arrayDePessoas.filter((pessoa) =>{
+      return pessoa.idade >= 18
+   })
+   return novo
 }
 
 // Exercício 16, letra B
 
 function menoresDe18(arrayDePessoas) {
-   // implemente sua lógica aqui
+   const novoMenores = arrayDePessoas.filter((pessoa) =>{
+      return pessoa.idade < 18
+   })
+   return novoMenores
 }
 
 // Exercício 17, letra A
-
+const array = [-1, 1, 2, 5, 25, 4]
 function multiplicaArrayPor2(array) {
-   // implemente sua lógica aqui
+   const novo = array.map((num) =>{
+      return num * 2
+   })
+      return novo
+
 }
 
 // Exercício 17, letra B
 
 function multiplicaArrayPor2S(array) {
-  // implemente sua lógica aqui
+  const novo = array.map((num) =>{
+     return (num * 2).toString()
+  })
+  return novo
 }
 
 // Exercício 17, letra C
 
 function verificaParidade(array) {
-   // implemente sua lógica aqui
-}
+      const novo = array.map((num) => {
+        if (num % 2 === 0) {
+          return `${num} é par`
+        } else {
+          return `${num} é ímpar`
+        }
+      })
+    
+      return novo
+   }
+
 
 // Exercício 18
 
@@ -176,30 +275,77 @@ const pessoas = [
 
 //Exercício 18, letra A
 
-function retornaPessoasAutorizadas(pessoas) {
-   // implemente sua lógica aqui
+function retornaPessoasAutorizadas() {
+   let pessoasAutorizadas =[]
+   for (const pessoa of pessoas){
+      if (pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60){
+         pessoasAutorizadas.push(pessoa)
+      }
+   }
+   return pessoasAutorizadas
 }
 
 
 // Exercício 18, letra B
 
-function retornaPessoasNaoAutorizadas(pessoas) {
-   // implemente sua lógica aqui
+function retornaPessoasNaoAutorizadas() {
+   let naoAutorizadas =[]
+   for (const pessoa of pessoas){
+      if (pessoa.altura < 1.5 || pessoa.idade < 14 || pessoa.idade > 60){
+         naoAutorizadas.push(pessoa)
+      }
+   }
+      return naoAutorizadas
 }
 
 //Exercício 19
 
 const consultas = [
-  { nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
-  { nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
-  { nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
-  { nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
-  ]
+   { nome: "João", genero: "masculino", cancelada: false, dataDaConsulta: "01/10/2019" },
+   { nome: "Pedro", genero: "masculino", cancelada: true, dataDaConsulta: "02/10/2019" },
+   { nome: "Paula", genero: "feminino", cancelada: false, dataDaConsulta: "03/11/2019" },
+   { nome: "Márcia", genero: "feminino", cancelada: true, dataDaConsulta: "04/11/2019" }
+ ]
 
-function retornaEmailConsulta(consultas) {
-  // implemente sua lógica aqui
-}
+function retornaEmailConsulta() {
+   return consultas.map((consulta) => {
+      let emailFinal = "Olá, "
+  
+      if (!consulta.cancelada) {
+        if (consulta.genero === "feminino") {
+          emailFinal += "Sra. "
+        } else {
+          emailFinal += "Sr. "
+        }
+  
+        emailFinal += consulta.nome + ". "
+        emailFinal += "Estamos enviando esta mensagem para "
+  
+        if (consulta.genero === "feminino") {
+          emailFinal += "lembrá-la "
+        } else {
+          emailFinal += "lembrá-lo "
+        }
+  
+        emailFinal += "da sua consulta no dia " + consulta.dataDaConsulta + ". "
+        emailFinal += "Por favor, acuse o recebimento deste-email."
+      } else {
+        if (consulta.genero === "feminino") {
+          emailFinal += "Sra. "
+        } else {
+          emailFinal += "Sr. "
+        }
+  
+        emailFinal += consulta.nome + ". "
+        emailFinal += "Infelizmente sua consulta marcada para o dia "
+        emailFinal += consulta.dataDaConsulta + " foi cancelada. "
+        emailFinal += "Se quiser, pode entrar em contato conosco para remarcá-la."
+      }
+      return emailFinal
+    })
+  }
 
+console.log(retornaEmailConsulta)
 //Exercício 20
 
 const contas = [
@@ -212,5 +358,12 @@ const contas = [
 ]
 
 function atualizaSaldo() {
-  // implemente sua lógica aqui
-}
+   contas.forEach((conta) => {
+      let totalDeCompras = 0
+      conta.compras.forEach((valor) => {
+        totalDeCompras += valor
+      })
+      conta.saldoTotal -= totalDeCompras
+    })
+    return contas
+  }
