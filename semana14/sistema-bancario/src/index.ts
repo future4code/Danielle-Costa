956e61928135d6e3dc8ec16b5f612eb11a32462a
Danielle Costa <dascott.pucca@gmail.com>
2021-05-22 05:10:38 -0300

    sistema bancario com os desafios

--- semana14/sistema-bancario/src/index.ts
+++ semana14/sistema-bancario/src/index.ts
@@ -1,36 +1,44 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
 import {accounts} from './accounts'
-
+import {Boleto, users} from './types'
 
 const app:Express = express()
 app.use(express.json())
 app.use(cors())
 
+const anoAtual = new Date().getFullYear();
+
 app.post('/users/create', (req:Request, res: Response) => {
     try{
-    const {name, CPF, dataNascAsString} = req. body
+    const {name, CPF, nasc} = req. body
     
-    const [dia, mes, ano] = dataNascAsString.split('/')
-
-    const dataNasc: Date = new Date(`${ano}-${mes}-${dia}`)
-
-    const idadeEmMilisegundos: number = Date.now() - dataNasc.getTime()
-    const idadeEmAnos: number= idadeEmMilisegundos / 1000 / 60 / 60 / 24 / 365
-    // 1000idade em segundos 60 em minutos 60 em horas 24 em dias 365 em anos
+    const anoNasc = nasc.substr(0, 4)
+    const idadeEmAnos = Number(anoAtual) - Number(anoNasc)
 
     if (idadeEmAnos < 18 ){
         res.statusCode = 406
         throw new Error("Usuario menor de 18 anos, não pode abrir conta")
     }
 
+    if (!name || !CPF) {
+     throw new Error(
+       "Necessario completar Name, CPF e Data de Nascimento"
+     );
+   }
+   users.map((users) => {
+    if (users.CPF.includes(CPF)) {
+      throw new Error("CPF já registrado.");
+    }
+  });
     accounts.push({
         name,
         CPF,
-        dataNasc,
+        nasc,
         saldo: 0,
         extrato:[]
    } )
+
 
    res.status(201).send('Conta criada com sucesso')
 }catch(error){
@@ -52,6 +60,69 @@ app.get('/users/all', (req:Request, res: Response) =>{
         res.send(error.message)
     }
 })
+
+app.get ('users/:CPF', (req:Request, res: Response) => {
+    try {
+    const paramsCPF = req.params.CPF as string;
+    let saldo;
+    if (paramsCPF.length < 11) {
+    throw new Error("Informe um cpf valido com 11 digitos")
+}else {
+    users.map((users) => {
+        if (users.CPF === paramsCPF) {
+            saldo = Number(users.saldo)
+        }
+    })
+    if (!saldo) {
+        throw new Error ('CPF não cadastrado')
+    }
+    res.status(200).send({ saldo });
+}
+} catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+}
+
+)
+
+app.put("/users/add", (req: Request, res: Response) => {
+    try {
+      const cpfQuery = req.query.cpf as string;
+      const nameQuery = req.query.name as string;
+      const valueQuery = req.query.value;
+      let result = false;
+      if (cpfQuery.length < 11 || !nameQuery || !valueQuery) {
+        throw new Error("Verifique os dados corretamente");
+      } else {
+        users.map((user) => {
+          if (
+            user.CPF === cpfQuery &&
+            user.name.toLowerCase() === nameQuery.toLocaleLowerCase()
+          ) {
+            user.saldo += Number(valueQuery);
+            let todayDate = new Date();
+            let newItem: Boleto = {
+              valor: Number(valueQuery),
+              dataVenc: todayDate.toISOString().substring(0, 10),
+              descrição: "Deposito em dinheiro",
+            };
+            user.extrato.push(newItem);
+            result = true;
+          }
+        });
+        if (!result) {
+          throw new Error("Usuario não encontrado.");
+        }
+        res.status(200).send("sucesso.");
+      }
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+  });
 
 app.listen(3003, () => {
     console.log('Server is running at port 3003')
